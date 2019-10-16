c5dd35dc11ad533c4104499814fb873d993aded6
gppatro <gabrielpp@al.insper.edu.br>
2019-10-16 21:17:50 +0000

    teste

--- firstApp/index.js
+++ firstApp/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 const port = 3000
-app.get(/teste', (req, res) => res.send(Meu primeiro contato com a nuvem!!!'))
+app.get('/teste', (req, res) => res.send('Meu primeiro contato com a nuvem!!!'))
 app.listen(port, () => console.log(`App listening on port ${port}!`))
